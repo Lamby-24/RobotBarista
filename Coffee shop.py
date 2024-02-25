c181423efa4f1d3fe71e6ac02f1f093934a587d2
Lamby-24 <mike_stacey@hotmail.co.uk>
2024-02-25 21:22:34 +0000

    added more if/else statements

--- Coffee shop.py
+++ Coffee shop.py
@@ -2,10 +2,24 @@
 print("Hello, welcome to Mikes Coffee")
 name = input("What is your name? \n")
 
-
-
-if name == "Ben":
-  print("You're not welcome hear evil Ben, get out!")
+if name == "Ben" or name == "Patricia" or name == "Loki":
+  evil_status = input("Are you evil?\n")
+  good_deed_status = int(input("How many good deeds have you done today?\n")) # Convert input to integer
+  if evil_status == "Yes" and good_deed_status < 4:
+    print("You're not welcome here " + name + ". Get out!")
+    exit()
+  else:
+    print ("Hello " + name + ", thank you so much for coming in today.\n")
+    menu = """Black Coffee, 
+Latte,
+Cappuccino.\n"""
+  print(name + ", What would you like from our menu today? Here is what we are serving\n" + menu)
+  order = input()
+  price = 8
+  quantity = input("How many coffees would you like?\n") # Convert input to integer
+  total = price * int(quantity) # Convert input to integer
+  print("Sounds good " + name + ", we'll have your " + quantity + " " + order + "'s ready for you in a moment.\n")
+  print("Here you go " + name +  ", that will be " + "£" + str(total) + " please.\n")
 else:
   print("Hello " + name + ", thank you so much for coming in today.\n")
   menu = """Black Coffee, 
