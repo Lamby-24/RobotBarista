a3fff3813f1ce91b494d44076f952c4c361dbb95
Lamby-24 <mike_stacey@hotmail.co.uk>
2024-02-26 16:21:02 +0000

    clear and reomve methods

--- camping.py
+++ camping.py
@@ -1,10 +1,8 @@
 camp_list = ["tent", "sleeping bag", "food", "water", "cooker"]
 camp_site = ["Postern Hill", 404, 27.7, True]
-me = (camp_list)
-you = (camp_list)
 #camp_list.extend(["toilet paper, " + "bidet"])
 camp_list.insert(0, "bidet")
 camp_list.insert(-1, "toilet paper")
-#print(me[4])
-#print(you[-2])
-print(camp_list)
+print(camp_list.pop(1))
+camp_list.pop(2)
+print(camp_list) 
