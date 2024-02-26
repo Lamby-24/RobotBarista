9ba264f72661108b420bb1635cd934b7ce456cb1
Lamby-24 <mike_stacey@hotmail.co.uk>
2024-02-26 00:30:26 +0000

    Lists and addtions to lists

--- camping.py
+++ camping.py
@@ -2,5 +2,9 @@ camp_list = ["tent", "sleeping bag", "food", "water", "cooker"]
 camp_site = ["Postern Hill", 404, 27.7, True]
 me = (camp_list)
 you = (camp_list)
-print(me[4])
-print(you[-2])
+#camp_list.extend(["toilet paper, " + "bidet"])
+camp_list.insert(0, "bidet")
+camp_list.insert(-1, "toilet paper")
+#print(me[4])
+#print(you[-2])
+print(camp_list)
